a9a7cd5671d0111e536bcfb3d6fa52ff79c59a6f
DragonGod65 <102479789+DragonGod65@users.noreply.github.com>
2023-01-27 17:59:23 +0100

    Files

--- src/main/java/net/dragongod65/void_worlds/DataLoader.java
+++ src/main/java/net/dragongod65/void_worlds/DataLoader.java
@@ -3,4 +3,6 @@ package net.dragongod65.void_worlds;
 public class DataLoader {
 
     //Loads the DataPackFiles
+    //register a namespace optimumwilds??
+    //Probably with the mcmeta file
 }
---
+++ src/main/java/net/dragongod65/void_worlds/RessourcePackLoader.java
@@ -0,0 +1,9 @@
+package net.dragongod65.void_worlds;
+
+public class RessourcePackLoader {
+
+    //Loads the RessourcePack Files
+    //register a namespace optimumwilds??
+    //Register different Types of RessourcePacks
+    //Probably with the mcmeta file
+}
